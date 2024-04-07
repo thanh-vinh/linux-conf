bd82189703bb69b758ca6587a5fa1da48fde361c
Vinh <thanh.vinh@hotmail.com>
2024-04-07 22:52:37 +0700

    fish: update `gl` alias

--- home/.config/fish/functions/gl.fish
+++ home/.config/fish/functions/gl.fish
@@ -1,5 +1,6 @@
 function gl --wraps=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\' --description alias\ gl=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\'
   # git log --pretty=format:'%h|%an|%s' -10 | column -t -s '|' $argv
-  git log --oneline $argv
+  # git log --oneline $argv
+git log --pretty="%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s" --date=short -10 $argv
         
 end
