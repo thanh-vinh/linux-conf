05c678480325eee4b2b8c52b7c5e7ce32d00f117
Vinh <thanh.vinh@hotmail.com>
2024-10-29 22:16:04 +0700

    fish: prettier git log, add `l=layzygit` alias

--- home/.config/fish/functions/gl.fish
+++ home/.config/fish/functions/gl.fish
@@ -1,6 +1,3 @@
 function gl --wraps=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\' --description alias\ gl=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\'
-  # git log --pretty=format:'%h|%an|%s' -10 | column -t -s '|' $argv
-  # git log --oneline $argv
-git log --pretty="%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s" --date=short -10 $argv
-        
+    git log --decorate --graph --pretty="%C(yellow)%h %C(reset)%s — %C(green)%an %C(reset)%C(dim)%cr%C(reset) %C(red)%C(bold)%d" $argv
 end
---
+++ home/.config/fish/functions/l.fish
@@ -0,0 +1,4 @@
+function l --wraps=lazygit --description 'alias l=lazygit'
+  lazygit $argv
+        
+end
