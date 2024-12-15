f7fe57ce4faf61b266b12c08cfed6a23e65c5cd2
Vinh <thanh.vinh@hotmail.com>
2024-12-15 14:01:53 +0700

    i3: update

--- home/.config/fish/functions/gl.fish
+++ home/.config/fish/functions/gl.fish
@@ -1,3 +1,3 @@
 function gl --wraps=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\' --description alias\ gl=git\ log\ --pretty=format:\'\%h\|\%an\|\%s\'\ -10\ \|\ column\ -t\ -s\ \'\|\'
-    git log --decorate --graph --pretty="%C(yellow)%h %C(reset)%s — %C(green)%an %C(reset)%C(dim)%cr%C(reset) %C(red)%C(bold)%d" $argv
+    git log --decorate --graph --pretty="%C(yellow)%h %C(reset)%s — %C(green)%an %C(reset)%C(dim)%cr%C(reset) %C(red)%C(bold)%d" -10 $argv
 end
